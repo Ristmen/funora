0d345f3ea013818747848b3eb471fa4e83741a86
Ristmen <yula1334@gmail.com>
2025-06-13 21:13:55 +0300

    Delete java/com/pb directory

--- java/com/pb/funora/core/model/FaqItem.kt
+++
@@ -1,6 +0,0 @@
-// FaqItem.kt
-package com.pb.funora.core.model
-data class FaqItem(
-    val question: String,
-    val answer: String
-)
--- java/com/pb/funora/core/model/Tournament.kt
+++
@@ -1,7 +0,0 @@
-// Tournament.kt
-package com.pb.funora.core.model
-data class Tournament(
-    val name: String,
-    val date: String,
-    val participants: Int
-)
